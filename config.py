b8ca46ce16ad389237ac6b4c407fef3a2104d6bc
Yung How <81214607+TeeYH12345@users.noreply.github.com>
2022-08-30 14:19:25 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cw5ohclgtk6u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "teeyunghow-employee"
+customregion = "us-east-1"
